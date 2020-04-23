f46cd1a50c11e1142e3d1b0427fae0f1d8774788
cjseaman <collin2009@gmail.com>
2020-04-23 17:09:15 +0100

    updated scripts to work with python3.7 Also changed saved directory for graph html file to /var/www/html/index.html

--- python/graph.py
+++ python/graph.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import os, pickle, random
 
 saved_graph = '.graphInfo'
+graph_location = '/var/www/html/index.html'
 
 def getPacketLoss():
-	return random.randint(0,50)
+    return random.randint(0,50)
 
 def generate():
 
@@ -15,7 +16,7 @@ def generate():
     graphInfo = {
                 'x_vals': [],
                 'y_vals': [],
-                'z_vals': []
+                'z_vals': [],
                 }
 
     if os.path.exists(saved_graph):
@@ -24,44 +25,43 @@ def generate():
 
     while True:
             
-            response = raw_input("(1) Press Enter to scan\n(2) Type \"calibrate\" to recalibrate distance\n(3) Type \"clear\" to clear previous scan results\n(4) Type \"quit\" to exit\n")
-            if response == 'calibrate' or response == '2':
-                calibrate()
-                continue
+        response = input("(1) Press Enter to scan\n(2) Type \"calibrate\" to recalibrate distance\n(3) Type \"clear\" to clear previous scan results\n(4) Type \"quit\" to exit\n")
+        if response == 'calibrate' or response == '2':
+            calibrate()
+            continue
 
-            if response == 'clear' or response == '3':
-                graphInfo = {
-                        'x_vals': [],
-                        'y_vals': [],
-                        'z_vals': []
-                        }
-                print("Graph has been reset")
-                continue
-            
-            if response == 'quit' or response == '4':
-                return
-            
-            x,y = triangulate()
+        if response == 'clear' or response == '3':
+            graphInfo = {
+                    'x_vals': [],
+                    'y_vals': [],
+                    'z_vals': []
+                    }
+            print("Graph has been reset")
+            continue
+        
+        if response == 'quit' or response == '4':
+            return
+        
+        x,y = triangulate()
 
-            if len(graphInfo['x_vals']) > 0:
-                if (x > (2*(sum(graphInfo['x_vals'])/len(graphInfo['x_vals']))) or y > (2*(sum(graphInfo['y_vals'])/len(graphInfo['y_vals'])))):
-                    print("Outlier detected")
-                    continue
+        if len(graphInfo['x_vals']) > 0:
+            if (x > (2*(sum(graphInfo['x_vals'])/len(graphInfo['x_vals']))) or y > (2*(sum(graphInfo['y_vals'])/len(graphInfo['y_vals'])))):
+                print("Outlier detected")
 
-            z = getPacketLoss()
-            graphInfo['x_vals'].append(x)
-            graphInfo['y_vals'].append(y)
-            graphInfo['z_vals'].append(z)
-            print(graphInfo)
-            
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(x = graphInfo['x_vals'], y = graphInfo['y_vals'], mode="markers"))
-            refresh_interval = 7
-            reload_script = 'window.onload=function() {window.setInterval(function() {window.location.reload(true)}, 1000 * ' + str(refresh_interval) + ')}'
-            fig.write_html('graph.html', auto_open = False, post_script = reload_script)
-            
-            with open(saved_graph, 'wb') as f:
-                pickle.dump(graphInfo, f)
+        z = getPacketLoss()
+        graphInfo['x_vals'].append(x)
+        graphInfo['y_vals'].append(y)
+        graphInfo['z_vals'].append(z)
+        print(graphInfo)
+        
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x = graphInfo['x_vals'], y = graphInfo['y_vals'], mode="markers", marker = dict(color=graphInfo['z_vals'])))
+        refresh_interval = 7
+        reload_script = 'window.onload=function() {window.setInterval(function() {window.location.reload(true)}, 1000 * ' + str(refresh_interval) + ')}'
+        fig.write_html(graph_location, auto_open = False, post_script = reload_script)
+        
+        with open(saved_graph, 'wb') as f:
+            pickle.dump(graphInfo, f)
 
 if __name__ == '__main__':
     generate()
--- python/signalLevel.py
+++ python/signalLevel.py
@@ -4,42 +4,42 @@ import subprocess
 
 OOR = "Out of Range"
 ips = ['192.168.4.2', '192.168.4.3']
-addresses = ['0A:BD:55:EC:5E:C4','D2:09:30:9D:F6:23']
+addresses = ['LeaPiNode1', 'LeaPiNode2']
 interface = 'wlan1'
 
 
 def getSignal(address):
-	scanCommand = ['sudo', 'iwlist', interface, 'scan']
-	lines = [' ',' ',' ',' ']
-        Signal = {
-            'strength': 0,
-            'address': address
-        }
-	print("Scanning...")
-	
-        scan = subprocess.Popen(scanCommand,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-	stdout,stderr = scan.communicate()
-	
-        lines = stdout.split('\n')
+    scanCommand = ['sudo', 'iwlist', interface, 'scan']
+    lines = [' ',' ',' ',' ']
+    Signal = {
+        'strength': 0,
+        'address': address
+    }
+    print("Scanning...")
+    
+    scan = subprocess.Popen(scanCommand,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout,stderr = scan.communicate()
+    
+    lines = stdout.decode().split('\n')
 
-        i = 0
-        for line in lines:
-            if(address in line):
-                Signal = {
-                    'strength': lines[i + 3].split('=')[1].split('/')[0],
-                    'address': address
-                }
-            i = i + 1
+    i = 0
+    for line in lines:
+        if(address in line):
+            Signal = {
+                'strength': lines[i - 2].split('=')[1].split('/')[0],
+                'address': address
+            }
+        i = i + 1
 
-	return Signal
+    return Signal
 
 def getAllSignals():
 
-	all_signals = []
-	for add in addresses:
-                signal = getSignal(add)
-		all_signals.append(signal)
-	return all_signals
+    all_signals = []
+    for add in addresses:
+        signal = getSignal(add)
+        all_signals.append(signal)
+    return all_signals
 
 if __name__ == '__main__':
-	print(getAllSignals())
+    print(getAllSignals())
